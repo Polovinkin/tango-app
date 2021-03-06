3a7e1ef19a90928cff55dc55d043deb47dd0900c
Dmitry Polovinkin <polovinkin@M1-MacBook-Air.local>
2021-03-06 12:46:36 +0300

    Docker and working main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +6,6 @@ app = Flask(__name__)
 @app.route("/ping")
 def ping():
   return "pong"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
